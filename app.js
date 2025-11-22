2f2c67d211f2ff566efda149ddb79ab404397716
matheus1336 <99938274+matheus1336@users.noreply.github.com>
2025-11-21 21:43:39 -0300

    Create BabyMonitor component in app.js
    Added initial structure for BabyMonitor component.

---
+++ app.js
@@ -0,0 +1,10 @@
+// Cole aqui TODO o código React do artifact anterior
+// (É o código completo da Babá Eletrônica que criamos)
+
+const { useState, useEffect, useRef } = React;
+
+function BabyMonitor() {
+  // ... TODO O CÓDIGO DO ARTIFACT AQUI ...
+}
+
+ReactDOM.render(<BabyMonitor />, document.getElementById('root'));
